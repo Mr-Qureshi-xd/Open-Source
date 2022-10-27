c64a1ad42ad4d3381bc6164361555985b611ca68
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2022-10-27 18:53:35 +0500

    Update and rename run.py to Dump.py

--- Dump.py
+++ Dump.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #encoding=utf:8
 #dumpingtool 0.0.1
-
+#--Credit Bilal Haider <3
 #---------[ IMPORTING MODULES ]-------->>
 from os import system as cmd
 from string import whitespace
